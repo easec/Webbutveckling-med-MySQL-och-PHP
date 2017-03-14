5cf3e9e67602f857b7c9a7e0e56d02c42a3f73a3
easec <mats.johannesson@easec.se>
2017-03-14 22:53:34 +0100

    Create 1-1.php

---
+++ Modul 3/1-1.php
@@ -0,0 +1,12 @@
+<?php
+    $txt1 = "Skoj att programmera i ";
+    $txt2 = "PHP";
+    $x = 5;
+    $y = 4;
+    print "$txt1$txt2";
+    print "\n";
+    print "Skoj med $txt2";
+    print "\n";
+    print $x + $y;
+    print "\n";
+?>
