0b1c510e5c80c57d1747af484058934dd8dbe969
easec <mats.johannesson@easec.se>
2017-03-14 23:07:10 +0100

    Create demo_switch.php

---
+++ Modul 3/demo_switch.php
@@ -0,0 +1,16 @@
+<?php
+	$myTest = "Hello2";
+	switch ($myTest) {
+		case "Hello1":
+			echo "Hello1\n";
+		break;
+		case "Hello2":
+			echo "Hello2\n";
+		break;
+		case "Hello3":
+			echo "Hello3\n";
+		break;
+		default: echo "Varkens Hello1, Hello2 eller Hello3";
+	}
+
+?>
