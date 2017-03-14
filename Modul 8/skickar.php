0eba837868f1deb5625c95de46fe16c834b3593a
easec <mats.johannesson@easec.se>
2017-03-14 22:44:13 +0100

    Create skickar.php

---
+++ Modul 8/skickar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <title>Extra övning formulär</title>
+</head>
+<body>
+    <form action="taremot.php" method="get">
+        <fieldset>
+            <legend>Formulär</legend>
+            <p>
+              <label for="city">Stad att åka till:</label>
+              <input type="text" name="city" id="city">
+            </p>
+            <p>
+              <button name="submit" type="submit">Skicka</button>
+            </p>
+        </fieldset>
+    </form>
+</body>
+</html>
