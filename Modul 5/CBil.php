0b6d854268b1351f1be5b8d7ee2ff24f7245aebd
easec <mats.johannesson@easec.se>
2017-03-15 15:54:53 +0100

    Create CBil.php

---
+++ Modul 5/CBil.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * CBil short summary.
+ *
+ * CBil description.
+ *
+ * @version 1.0
+ * @author ditt_namn
+ */
+class CBil
+{
+    // Medlemsvariabler
+
+    public $färg;
+    public $märke;
+
+    // Metoder
+
+    public function accelerera()
+    {
+        echo 'Accelererar...';
+    }
+
+    public function bromsa()
+    {
+        echo 'Sänker farten...';
+    }
+
+    public function svänga()
+
+    {
+        echo 'Svänger...';
+    }
+}
+
+?>
