f9a2a1680b0e740af16afd418ec40c24276cac36
easec <mats.johannesson@easec.se>
2017-03-16 15:26:56 +0100

    Update custom_error_handler.php

--- Modul 6/custom_error_handler.php
+++ Modul 6/custom_error_handler.php
@@ -1,6 +1,8 @@
+<?php  header('Content-Type: text/html; charset=utf-8'); ?>
 <!DOCTYPE html>
-<html>
+<html lang="sv">
 <head>
+    <meta charset="utf-8">
     <title></title>
     <style type="text/css">
         .notice {
@@ -24,35 +26,28 @@
 <body>
     <?php // olika fel som skickas till skräddarsydd felhanterare
     set_error_handler('myHandler');
-
     // Rapportera alla fel
     error_reporting(E_ALL);
-
     // Generera fel
     echo $var;      // notice
     echo 25/0;      // warning
-
     // custom error handler
     function myHandler($type, $msg, $file, $line, $context)
     {
-        $text = "Ett fel uppstod på rad $line när server skulle bearbeta din begäran.<p>
+        $text = "Ett fel uppstod på rad $line när server skulle bearbeta din begäran.<br />
 	<p><p>
         Felmeddelande: $type $msg<p>
 	Var snäll och besök vår <a href=https://easec.se>hemsida</a> och försök igen.";
-
-
         switch($type) {
             case E_NOTICE:
-                echo "<div class=\"notice\">$text</div><p>";
+                echo "<div class=\"notice\">$text</div><br />";
                 break;
             case E_WARNING:
-                echo "<div class=\"warning\">$text</div><p>";
+                echo "<div class=\"warning\">$text</div><br />";
                 break;
             case E_ERROR:
-                echo "<div class=\"error\">$text</div><p>";
+                echo "<div class=\"error\">$text</div><br />";
                 break;
         }
     }
-    ?>
-</body>
-</html>
+    ?>  
