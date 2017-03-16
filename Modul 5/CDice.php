55a8215b6818d2a1a470376e20f845ec69def7d4
easec <mats.johannesson@easec.se>
2017-03-16 13:49:42 +0100

    Create CDice.php

---
+++ Modul 5/CDice.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This is a summary.
+ * This is a description
+ */
+
+
+class CDice
+{
+
+
+    public $rolls = array();
+
+    public function Roll($times) {
+
+        $this->rolls = array();
+
+        for($i = 0; $i < $times; $i++) {
+            $this->rolls[] = rand(1, 6);
+        }
+    }
+
+ 
+    public function GetTotal() {
+        return array_sum($this->rolls);
+    }
+
+    public function GetAverage() {
+        return round(array_sum($this->rolls) / count($this->rolls), 1);
+    }
+
+}
