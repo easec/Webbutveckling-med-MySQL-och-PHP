2f0966d18b431f93deea2a23442f75d5eda439d0
easec <mats.johannesson@easec.se>
2017-03-17 12:27:23 +0100

    Update skickameddelande.php

--- Modul 10/skickameddelande.php
+++ Modul 10/skickameddelande.php
@@ -1,44 +1,36 @@
+<!DOCTYPE html>
+
+<meta http-equiv='Content-type' content='text/html; charset=ISO-8859-1' />
 <html>
-<head>
-</head>
-<body>
+        <head>
+        </head>
+    <body>
 
-<?php
-
-$servername = "localhost";
-
-$username = "root";
-
-$password = "Pa$$w0rd";
-
-$dbname = "kontakt_db";
+        <?php
 
 
-// Skapa anslutning
-$conn = new mysqli($servername, $username, $password, $dbname);
+            // Skapa anslutning
 
 
-// Kontrollera anslutning
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+            $conn = new mysqli('localhost', 'root', 'Pa$$w0rd', 'kontakt_db');
+
+            // Kontrollera anslutning
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $anv_Namn = htmlspecialchars($_POST['anv_Namn']);
+            $anv_Mail = htmlspecialchars($_POST['anv_Mail']);
+            $anv_Meddelande = htmlspecialchars($_POST['anv_Meddelande']);
+            echo $anv_Namn . "<br />";
+            echo $anv_Mail . "<br />";
+            echo $anv_Meddelande . "<br />";
+            mysqli_query($conn, "INSERT INTO meddelande_tbl (`anv_Namn`, `anv_Mail`, `anv_Meddelande`) VALUES ('$anv_Namn', '$anv_Mail', '$anv_Meddelande')");
 
 
-
-$anv_Namn = htmlspecialchars($_POST['anv_Namn']);
-$anv_Mail = htmlspecialchars($_POST['anv_Mail']);
-$anv_Meddelande = htmlspecialchars($_POST['anv_Meddelande']);
-echo $anv_Namn . "<br />";
-echo $anv_Mail . "<br />";
-echo $anv_Meddelande . "<br />";
-mysqli_query($conn, "INSERT INTO meddelande_tbl (`anv_Namn`, `anv_Mail`, `anv_Meddelande`) VALUES ('$anv_Namn', '$anv_Mail', '$anv_Meddelande')");
-
-
-
-
-$conn->close(); 
-header("Location: formkontakt.html");  
-?>
+            $conn->close(); 
+            header("Location: formkontakt.html");  
+        ?>
 
 
 </body>
