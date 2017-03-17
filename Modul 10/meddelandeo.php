309d8c219f195ef4ad8d186a19013e802deb4685
easec <mats.johannesson@easec.se>
2017-03-17 03:10:11 +0100

    Create meddelandeo.php

---
+++ Modul 10/meddelandeo.php
@@ -0,0 +1,44 @@
+<?php
+// Ansluta till databas
+$servername = "localhost";
+$username = "root";
+$password = "Pa$$w0rd";
+$dbname = "kontakt_db";
+// Kontrollera anslutningen och exekvera SQL
+try { 
+    	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$sql = 'SELECT * FROM meddelande_tbl';
+	$q = $conn->query($sql);
+	$q->setFetchMode(PDO::FETCH_ASSOC);
+}
+catch(PDOException $e)
+    	{
+		die("Anslutning misslyckades: " . $e->getMessage());
+    	}
+$conn = null;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div id="meddelande">
+	<h1>Meddelande</h1>
+	<?php while ($row = $q->fetch()): ?>
+	<br />
+	Användare: <?php echo $row["anv_Namn"]; ?>
+	<br />
+	E-post: <?php echo $row["anv_Mail"]; ?>	
+	<br />
+	Tid: <?php echo $row["anv_MeddTS"]; ?>	
+	<br />
+	Meddelande: <?php echo $row["anv_Meddelande"]; ?>	
+	<br />
+	<?php endwhile; ?>
+<br />
+Slut på poster!
+</div>
+</body>
+</html
