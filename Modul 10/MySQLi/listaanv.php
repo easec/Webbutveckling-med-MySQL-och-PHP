aad28ebd1dd594579cfe46a84faa233b8b28ddac
easec <mats.johannesson@easec.se>
2017-03-20 13:45:49 +0100

    Update listaanv.php

--- Modul 10/MySQLi/listaanv.php
+++ Modul 10/MySQLi/listaanv.php
@@ -1,50 +1,50 @@
 <!DOCTYPE html>
 <html lang="sv">
 <html>
-  <head>
-    <meta charset="UTF-8">
-  </head>
-  <body>
-  <h2>Användare</h2>
-  <?php
+<head>
+<meta charset="UTF-8">
+</head>
+<body>
+<h2>Användare</h2>
+<?php
 
-    $servername = 'localhost';
-    $username = 'root';
-    $password = 'Pa$$w0rd';
-    $dbname = 'kontakt_db';
+$servername = 'localhost';
+$username = 'root';
+$password = 'Pa$$w0rd';
+$dbname = 'kontakt_db';
 
 
 // Skapa anslutning
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    mysqli_set_charset($conn, "utf8");
+$conn = new mysqli($servername, $username, $password, $dbname);
+mysqli_set_charset($conn, "utf8");
+
+
 
 // Kontrollera anslutning
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    }
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
-  echo "<br />";
+echo "<br />";
 
-  $sql = "SELECT * FROM anv_tbl";
-  $result = $conn->query($sql);
+$sql = "SELECT * FROM anv_tbl";
+$result = $conn->query($sql);
 
-  if ($result->num_rows > 0) {
-  
-  // Lägg ut information för varje rad
+
+if ($result->num_rows > 0) {
+    // output data of each row
 
     while($row = $result->fetch_assoc()) {
 
-        echo "Id: " . $row["anv_id"] . "<br />" . "Förnamn: " . $row["fnamn"] . "<br />" . "   Efternamn: " . $row["enamn"] . "<br />" . "Adress: " . $row["adress1"] . "<br />" . "Postnummer: " . $row["pnummer"] . "  Postort : " . $row["postort"] . "<br />" . "E-post : " . $row["epost"] . "<br /><br />";
+        echo "Id: " . $row["anv_id"] . "<br />" . "Förnamn: " . $row["fnamn"] . "<br />" . "   Efternamn: " . $row["enamn"] . "<br />" . "Adress: " . $row["adress1"] . "<br />" . "Postnummer: " . $row["pnummer"] . "  Postort: " . $row["postort"] . "<br />" . "E-post: " . $row["epost"] . "<br /><br />";
 
     }
-  } else {
+} else {
     echo "0 results";
-  }
-  echo "<br />";
-  echo "Slut på poster!";
-  
-  // Stäng anslutningen
-  $conn->close();   
+}
+echo "<br />";
+echo "Slut på poster!";
+$conn->close();   
 ?>
 
 </body>
