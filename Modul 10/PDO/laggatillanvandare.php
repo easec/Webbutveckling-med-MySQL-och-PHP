ba4360725f9d6fd4f0a2251cba9e42b652a4b329
easec <mats.johannesson@easec.se>
2017-03-21 13:37:12 +0100

    Update laggatillanvandare.php

--- Modul 10/PDO/laggatillanvandare.php
+++ Modul 10/PDO/laggatillanvandare.php
@@ -13,9 +13,9 @@ $mtelefon = $_POST['mtelefon'];
 try { 
     	$conn = new PDO('mysql:host=localhost;dbname=kontakt_db;charset=utf8', 'root', 'Pa$$w0rd');
     	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $sql = "INSERT INTO anv_tbl (fnamn, enamn, adress1, adress2, adress3, pnummer, postort, epost, ftelefon, mtelefon) VALUES ('$fnamn', '$enamn', '$adress1', '$adress2', 'adress3', '$pnummer', '$postort', '$epost', 'ftelefon', 'mtelefon')";
-      $conn->exec($sql);
-      echo "Post inlagd!";
+      	$sql = "INSERT INTO anv_tbl (fnamn, enamn, adress1, adress2, adress3, pnummer, postort, epost, ftelefon, mtelefon) VALUES ('$fnamn', '$enamn', '$adress1', '$adress2', 'adress3', '$pnummer', '$postort', '$epost', 'ftelefon', 'mtelefon')";
+      	$conn->exec($sql);
+      	echo "Post inlagd!";
 }
 catch(PDOException $e)
     	{
